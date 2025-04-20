682cf32d933e69c9d90607267dc8060681f57f1a
Rohitbirhade0605 <rohitbirhade469@gmail.com>
2025-04-21 01:24:54 +0530

    finished the project, adding to local files

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,9 @@
 const input = document.querySelector("#search input");
 const button = document.querySelector("button");
 const taskBox = document.querySelector("#taskBox");
-
+const unchecked = document.querySelector(".box");
+const list = document.querySelector(".list");
+const remove = document.querySelector(".delete");
 
 
 
@@ -14,50 +16,114 @@ const taskBox = document.querySelector("#taskBox");
     function createTask(){
 
         const input = document.querySelector("#search input").value.trim();
+        const a = input.slice(0, 1).toUpperCase();
+        const b = input.slice(1,43);
+        const uppercaseInput = a + b;                      // Change first letter to UpperCase
+        
 
 
-        let adding = document.createElement("div");         // adding parent div inside taskBox 
+        let adding = document.createElement("div");        // adding parent div inside taskBox 
             adding.classList.add("tasks");
       
-        let img1 = document.createElement("img");          // adding img1 (unchecked) inside parent div
-            img1.classList.add("box");
-            img1.src="./images/unchecked.png";
+        let divImg = document.createElement("div");        // adding div with bg img (unchecked) inside parent div
+            divImg.classList.add("box");
       
-            adding.appendChild(img1);                     // append img1 to parent div
+            adding.appendChild(divImg);                    // append divimg to parent div
       
-        let childDiv = document.createElement("div");     // adding child Div to parent div
+        let childDiv = document.createElement("div");      // adding child Div to parent div
             childDiv.classList.add("list");
-            childDiv.textContent = input;                 // sets childDiv text content as input value
+            childDiv.textContent = uppercaseInput;         // sets childDiv text content as input value
       
             adding.appendChild(childDiv);                 
       
-        let img2 = document.createElement("img");        // adding img2 (delete) to parent div
-            img2.classList.add("delete");
-            img2.src="./images/delete.png";
+        let img = document.createElement("img");           // adding img (delete) to parent div
+            img.classList.add("delete");
+            img.src="./images/delete.png";
       
-            adding.appendChild(img2);
+            adding.appendChild(img);
       
             
       
-            taskBox.appendChild(adding);                 // adding parent div to our taskBox
+            taskBox.appendChild(adding);                   // adding parent div to our taskBox 
+            
+            saveData()                                     // saves data each time we create new set
+    }
+
       
-      }
+    
+    function saveData() {
+        localStorage.setItem("storage", (taskBox.innerHTML));       //Save data in form of func in local storage
+    }
+
+    function show(){
+        taskBox.innerHTML = localStorage.getItem("storage");        //Shows the data to browser saved in SaveData()
+    }
+
+
 
 
     // Event listners ---------
 
-    button.addEventListener("click", ()=> {             
-        createTask()   
-        input.value = "";
+    button.addEventListener("click", ()=> {                   
+        
+        if (input.value === "") {                                // handling empty input value
+            input.placeholder = "Please add your task first"    
+        }
+        else {
+            createTask()                                            
+            input.value = "";                                    //reset value and placeholder
+            input.placeholder = ""
+        }
     })
 
 
-    input.addEventListener("keydown", (e)=> {
-    if (e.key ==="Enter"){
-        createTask()
-        input.value = "";
+
+    input.addEventListener("keydown", (e)=> {                   
+      if (e.key ==="Enter"){                                     //if input key "Enter" then func              
+
+         if (input.value === "") {                               // handling empty input value
+            input.placeholder = "Please add your task first"
+         }
+         else {
+            createTask()                                         
+            input.value = "";                                    //reset value and placeholder
+            input.placeholder = ""
+         }
+    }
+    else {
     }
     })
 
 
 
+    taskBox.addEventListener("click", (e)=> {                    //target parent of selected child listner
+        
+       if (e.target.classList.contains("box")) {                 //action trigger if parent contains a child
+           e.target.classList.toggle("boxSwitch")
+           saveData()                                            // Saves data of eventlistner aswell
+       }
+       else if (e.target.classList.contains("delete")) {         
+                e.target.parentElement.remove();                 //delete the parent by target child elem
+                saveData()
+       }
+       else {
+
+       }
+    })
+
+    
+
+    show();                                                      // calls the local storage Getitem
+
+
+
+
+
+
+     
+       
+          
+ 
+
+   
+           
